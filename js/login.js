eff49a5e6174f7b13b55ad06b13af21a673abe98
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-06-23 22:52:15 +0530

    commiting login page

--- js/login.js
+++ js/login.js
@@ -4,3 +4,28 @@ function myLogin(){
 window.open("./login.html","","width=700px,height=1000px");
 
 }
+
+
+
+
+
+function loginFunction(){
+
+var userName = document.getElementById("loginname").value;
+var passWord = document.getElementById("loginpassword").value;
+
+if((userName != "Jiban Debashis") && (passWord != "bapu1234")){
+alert("The username or password is inncorrect");
+}
+
+if(userName != "Jiban Debashis"){
+document.getElementById("message").innerHTML="**Incorrect username";
+return false;
+}
+
+if(passWord != "bapu1234"){
+document.getElementById("messagess").innerHTML="**Incorrect password";
+return false;
+}
+
+  }
